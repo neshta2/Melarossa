75d59c64b059de58744deb7eaa65552b65125fe6
”neshta2” <neshta2@gmail.com>
2022-01-16 10:13:19 +0300

    16/01/2022 Реализована программа, которая может отсортировать массив целых чисел двумя способами: 1. алгоритм Сортировки пузырьком 2. алгоритм Сортировки выбором. С тестом.

--- src/StrategyClient/StrategyApp.java
+++ src/StrategyClient/StrategyApp.java
@@ -9,7 +9,7 @@ public class StrategyApp {
         sortStrategy.execute(arr0);
 
         int[] arr1={3,-8,2,0,33,1,3,2}; //Сортировка пузырьком
-        sortStrategy.sortSrategy(new BubbleSort());
+        sortStrategy.sortSrategy(new MetodsSort());
         sortStrategy.execute(arr1);
 
         /*int[] arr2={11,4,2,7,8,54};
@@ -18,18 +18,6 @@ public class StrategyApp {
         */
     }
 
-}
-
-class Sorter {
-    Sorting srategy;
-
-    public void sortSrategy(Sorting srategy) {
-        this.srategy = srategy;
-    }
-
-    public void execute(int[] arr) {
-        srategy.sort(arr);
-    }
 }
 
 interface Sorting {
---
+++ src/StrategyClient/Test/StrategyAppTest.java
@@ -0,0 +1,23 @@
+package StrategyClient.Test;
+
+import StrategyClient.MetodsSort;
+import StrategyClient.SelectionSort;
+import StrategyClient.Sorter;
+import org.junit.Test;
+
+
+
+public class StrategyAppTest {
+
+    @Test
+    public void main() {
+        Sorter sortStrategy = new Sorter();
+        int[] arr0={1,3,2,1};//Сортировка выбором
+        sortStrategy.sortSrategy(new SelectionSort());
+        sortStrategy.execute(arr0);
+
+        int[] arr1={3,-8,2,0,33,1,3,2}; //Сортировка пузырьком
+        sortStrategy.sortSrategy(new MetodsSort());
+        sortStrategy.execute(arr1);
+    }
+}
